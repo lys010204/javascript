051341ba384db879717156a7d3970c8d0898d496
lys010204 <129822388+lys010204@users.noreply.github.com>
2023-09-27 15:15:13 +0900

    Delete 4장 directory

--- 4장/js/rest2.js
+++
@@ -1,9 +0,0 @@
-// 인수의 일부분만 나머지 매개변수로 받기
-function fav(first, ...favs){
-    
-    let str1 = `가장 좋아하는 과일은 ${first}입니다.\n`;
-    let str2 = `나머지 과일은 ${favs}입니다.`;
-    return str1+str2;
-}
-
-console.log(fav("복숭아",'사과','배','수박','망고'));
--- 4장/js/scope-2.js
+++
@@ -1,7 +0,0 @@
-var hi = "Hello"; // 전역 변수
-
-function greeting() {
-    console.log(hi);
-}
-
-greeting();
