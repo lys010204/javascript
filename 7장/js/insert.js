69a4d6175d371e139b8d65dc514ffd1cf0ccfc6b
lys010204 <129822388+lys010204@users.noreply.github.com>
2023-10-18 05:51:47 +0000

    Delete 7장 directory

--- 7장/js/insert.js
+++
@@ -1,8 +0,0 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click",() =>{
-    let newP = document.createElement("p");
-    let textNode = document.createTextNode("안녕하세요");
-    newP.appendChild(textNode);
-    let basicNode = document.querySelectorAll("p")[2];
-    document.body.insertBefore(newP,basicNode);
-},{once:true});
